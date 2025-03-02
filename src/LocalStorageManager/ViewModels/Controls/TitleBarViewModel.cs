75937484b21a7bbd41828a4ce9197953133b4e01
Staryy-Dobryy <Lfcega1@gmail.com>
2025-03-02 23:40:09 +0200

    Added base VM for controls

---
+++ src/LocalStorageManager/ViewModels/Controls/TitleBarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalStorageManager.ViewModels.Controls
+{
+    public class TitleBarViewModel : ObservableObject
+    {
+        public string Title { get; private set; } = "Local Storage Manager";
+    }
+}
