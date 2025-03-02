5f33e0bd003b3ca626013a2174da542c51d4523e
Staryy-Dobryy <Lfcega1@gmail.com>
2025-03-02 23:41:51 +0200

    Added tool kit action view

---
+++ src/LocalStorageManager/Views/Controls/ToolKitAction/ToolKitAction.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LocalStorageManager.Controls;
+
+public partial class ToolKitAction : UserControl
+{
+    public ToolKitAction()
+    {
+        InitializeComponent();
+    }
+}
