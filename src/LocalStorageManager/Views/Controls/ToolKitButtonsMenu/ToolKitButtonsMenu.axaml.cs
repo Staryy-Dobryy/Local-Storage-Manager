f5bc644102fd890251ef6d298829f64c6b9af927
Staryy-Dobryy <Lfcega1@gmail.com>
2025-03-02 23:42:25 +0200

    Added tool view for buttons

---
+++ src/LocalStorageManager/Views/Controls/ToolKitButtonsMenu/ToolKitButtonsMenu.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LocalStorageManager.Controls;
+
+public partial class ToolKitButtonsMenu : UserControl
+{
+    public ToolKitButtonsMenu()
+    {
+        InitializeComponent();
+    }
+}
